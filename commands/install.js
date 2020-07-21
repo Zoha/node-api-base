06948003fad83f7129665ece737317fac9a498c7
Zoha Banam <Zoha.Banam@gmail.com>
2020-07-21 05:09:30 +0430

    add install comand

---
+++ commands/install.js
@@ -0,0 +1,50 @@
+const path = require("path")
+const fs = require("fs")
+const chalk = require("chalk")
+
+module.exports = {
+  command: "install <appName>",
+  options: ["-j, --just-env"],
+  action(appName, ctx) {
+    appName = appName.toLowerCase()
+    const packageJsonFilePath = path.join(__dirname, "../package.json")
+    const envExampleFilePath = path.join(__dirname, "../.env.example")
+    const envFilePath = path.join(__dirname, "../.env")
+    const configFilePath = path.join(__dirname, "../config.js")
+
+    if (!ctx.justEnv) {
+      fs.writeFileSync(
+        packageJsonFilePath,
+        fs
+          .readFileSync(packageJsonFilePath, "utf-8")
+          .replace('name": "base-api', `name": "${appName}-api`)
+          .replace(
+            '"description": "base project api app"',
+            `"description": "${appName} project api app"`
+          )
+      )
+
+      fs.writeFileSync(
+        envExampleFilePath,
+        fs
+          .readFileSync(envExampleFilePath, "utf-8")
+          .replace('27017/baseTest"', `27017/${appName}Test"`)
+          .replace('27017/base"', `27017/${appName}"`)
+      )
+
+      fs.writeFileSync(
+        configFilePath,
+        fs
+          .readFileSync(configFilePath, "utf-8")
+          .replace('27017/baseTest"', `27017/${appName}Test"`)
+          .replace('27017/base"', `27017/${appName}"`)
+      )
+
+      console.log(chalk.green("app name setting done"))
+    }
+
+    fs.copyFileSync(envExampleFilePath, envFilePath)
+
+    console.log(chalk.green("env.example file copied"))
+  }
+}
