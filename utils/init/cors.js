f310d52201a07df7287fb67c4bd385f96de10b09
Zoha Banam <Zoha.Banam@gmail.com>
2021-03-29 15:39:19 +0430

    fix cors

--- utils/init/cors.js
+++ utils/init/cors.js
@@ -1,12 +1,25 @@
 const config = require("@config")
 const cors = require("cors")
 
-module.exports = app => {
-  const whiteList = Array.isArray(config.accessOrigins)
-    ? config.accessOrigins
-    : [config.accessOrigins]
-  const corsOptions = {
-    origin: whiteList
+const whitelist = config.accessOrigins
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!Array.isArray(whitelist)) {
+      return whitelist === "*" || new RegExp(whitelist, "i").test(origin)
+        ? callback(null, true)
+        : callback(null, false)
+    }
+
+    if (whitelist.map(i => new RegExp(i, "i")).find(i => i.test(origin))) {
+      callback(null, true)
+    } else {
+      callback(null, false)
+    }
   }
+}
+
+module.exports.corsOptions = corsOptions
+
+module.exports = app => {
   app.use(cors(corsOptions))
 }
